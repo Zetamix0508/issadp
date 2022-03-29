66a0f693b4cee47432ee2715103b4bfa2de0587d
Zetamix0508 <102572199+Zetamix0508@users.noreply.github.com>
2022-03-29 01:04:00 -0500

    Proyecto Agregado el AdminLTE

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,38 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, odit quae. Tempora quae ad nemo repellendus, maiores fugiat eos quidem maxime voluptas aliquam neque molestiae, nihil minus esse, delectus beatae.</p>
+    
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
